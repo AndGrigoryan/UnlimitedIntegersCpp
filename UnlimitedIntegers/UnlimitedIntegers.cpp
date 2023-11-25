d415b1bc901b749d0d076f78f054c4534f757817
Andranik Grigoryan <andranik.grigoryan.02@gmail.com>
2023-11-25 19:09:40 +0400

    Implement subtraction operator in BigInt class

--- UnlimitedIntegers/UnlimitedIntegers.cpp
+++ UnlimitedIntegers/UnlimitedIntegers.cpp
@@ -8,6 +8,7 @@ class INumber
 public:
 	virtual void print() const = 0;
 	virtual INumber* operator+(const INumber& other) const = 0;
+	virtual INumber* operator-(const INumber& other) const = 0;
 };
 
 class BigInt : public INumber 
@@ -71,15 +72,69 @@ public:
 
 		return tmp;
 	}
+
+	INumber* operator-(const INumber& other) const override
+	{
+		const BigInt& otherBigInt = dynamic_cast<const BigInt&>(other);
+		std::string result = "";
+		int borrow = 0;
+
+
+		for (int i = 0; i < std::max(this->digits.size(), otherBigInt.digits.size()) || borrow; ++i)
+		{
+
+			if (i == result.size())
+			{
+				result.push_back('0');
+			}
+
+			int diff = borrow;
+
+			if (i < this->digits.size())
+			{
+				diff += int(this->digits[i] - '0');
+			}
+			if (i < otherBigInt.digits.size())
+			{
+				diff -= int(otherBigInt.digits[i] - '0');
+			}
+
+			if (diff < 0)
+			{
+				borrow = -1;
+			}
+			else
+			{
+				borrow = 0;
+			}
+
+			result[i] = (diff + 10) % 10 + '0';
+
+		}
+
+		while (result.size() > 1 && result.back() == '0')
+		{
+			result.pop_back();
+		}
+
+		std::reverse(result.begin(), result.end());
+
+		INumber* tmp = new BigInt(result);
+
+
+		return tmp;
+
+	}
+
 };
 
 int main()
 {
 
-	INumber* num1 = new BigInt("9999");
-	INumber* num2 = new BigInt("2006");          
+	INumber* num1 = new BigInt("200");
+	INumber* num2 = new BigInt("250");          
 
-	INumber* num3 = *num1 + *num2;
+	INumber* num3 = *num1 - *num2;
 
 	num1->print();
 	num2->print();
